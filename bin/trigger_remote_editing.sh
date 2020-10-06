8ade3911bcebbfc22e2457a214bd5f508850f00f
nightkhaos <nightkhaos+gpg@gmail.com>
2020-10-06 14:14:49 +1100

    script formatting

--- bin/trigger_remote_editing.sh
+++ bin/trigger_remote_editing.sh
@@ -17,16 +17,16 @@ trigger_remote_editing() {
   TS="["$(date "+%Y-%m-%d %H:%M:%S")"]"
   echo "${TS} Triggered: ""$@" >> ${LOGFILE}
   if [[ "${FTYPE}" == "directory" ]]; then
-        CMD="${VEDITOR} scp://${USER}@${MACHINE}/${FILEPATH}/"
-    elif [[ "${FTYPE}" == "file" ]]; then
-        CMD="${VEDITOR} scp://${USER}@${MACHINE}/${FILEPATH}"
-    else
-        echo "${TS} Error: Bad arguments." >> ${LOGFILE}
-        exit 1
-    fi
+    CMD="${VEDITOR} scp://${USER}@${MACHINE}/${FILEPATH}/"
+  elif [[ "${FTYPE}" == "file" ]]; then
+    CMD="${VEDITOR} scp://${USER}@${MACHINE}/${FILEPATH}"
+  else
+    echo "${TS} Error: Bad arguments." >> ${LOGFILE}
+     exit 1
+  fi
 
-    echo "${TS} ${CMD}" >> ${LOGFILE}
-    ${CMD}
+  echo "${TS} ${CMD}" >> ${LOGFILE}
+  ${CMD}
 }
 
 trigger_remote_editing "$@"
